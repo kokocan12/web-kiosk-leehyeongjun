ad96fa51c3ef92f16430eed8f2f3d01d65321a0b
이형준 <49009864+kokocan12@users.noreply.github.com>
2022-08-08 15:11:24 +0900

    useLocation, useNavigation hooks에 에러메시지 추가 (#31)

--- client/src/lib/router/hooks.tsx
+++ client/src/lib/router/hooks.tsx
@@ -1,14 +1,25 @@
+import { invariant } from '@lib/utils';
 import { useContext } from 'react';
 import { LocationContext, NavigationContext } from './context';
 
 export const useLocation = () => {
   const context = useContext(LocationContext);
 
+  invariant(
+    !context,
+    'Component is not child of <Router>, if you want to use useLocation hook, place component into <Router> ',
+  );
+
   return context.location;
 };
 
 export const useNavigation = () => {
   const context = useContext(NavigationContext);
+
+  invariant(
+    !context,
+    'Component is not child of <Router>, if you want to use useNavigation hook, place component into <Router> ',
+  );
 
   return context.navigate;
 };
