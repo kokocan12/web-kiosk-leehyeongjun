41ae89560b7dc137ec13fcbcacf650e6db6e0195
kokocan12 <sky203614@gmail.com>
2022-08-12 01:27:57 +0900

    config 변경 후 재빌드

--- client/src/api/config.ts
+++ client/src/api/config.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:3001/api';
+const baseURL = process.env.REACT_APP_BASE_URL;
 const instance = axios.create({
   baseURL,
   headers: { Accept: '*/*' },
